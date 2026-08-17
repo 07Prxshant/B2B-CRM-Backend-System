4dd5b30f6a6c632f0f2777e18a1554eb22b0902b
Prashant Kumar Gupta <kumarprashantgupta07@gmail.com>
2026-08-17 19:52:40 +0530

    added create follow up controller

---
+++ src/controllers/followup.controller.js
@@ -0,0 +1,73 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import Lead from "../models/lead.model.js";
+import User from "../models/user.model.js";
+import FollowUp from "../models/followup.model.js";
+import History from "../models/history.model.js";
+
+const createFollowUp = asyncHandler(async(req, res)=>{
+    const { leadId, assignedTo, followUpDate, note } = req.body;
+    if (!leadId || !assignedTo || !followUpDate || !note?.trim()) {
+        throw new ApiError(400, 'All fields are mandatory')
+    }
+
+    const lead = await Lead.findById(leadId)
+    if (!lead){
+        throw new ApiError(404,'Lead not found')
+    }
+    if (lead.organizationId.toString() !== req.user.organizationId.toString()){
+        throw new ApiError(400,'Invalid request')
+    }
+    if (lead.isActive !== true){
+        throw new ApiError(400,'Lead is inactive')
+    }
+
+    const assignedUser = await User.findById(assignedTo);
+    if (!assignedUser){
+        throw new ApiError(404,'User not found')
+    }
+    if (assignedUser.organizationId.toString() !== req.user.organizationId.toString()){
+        throw new ApiError(400,'Invalid request')
+    }
+    if (assignedUser.isActive !== true){
+        throw new ApiError(400,'Lead is inactive')
+    }
+
+    const followUp = await FollowUp.create({
+        organizationId: req.user.organizationId,
+        leadId,
+        assignedTo,
+        followUpDate,
+        note
+    })
+
+    await History.create({
+        organizationId:req.user.organizationId,
+        leadId : lead._id,
+        performedBy: req.user._id,
+        eventType: 'FOLLOWUP_CREATED',
+        description: `Follow-Up created by ${req.user._id}`
+    })
+
+    return res.status(201)
+    .json(new ApiResponse(201,followUp,'Follow-up crreated succesfully'))
+
+})
+
+const getAllFollowUp = asyncHandler(async(req, res)=>{
+
+})
+
+const getFollowUp = asyncHandler(async(req, res)=>{
+
+})
+
+const updateFollowUp = asyncHandler(async(req, res)=>{
+
+})
+
+export {
+    createFollowUp,
+    
+}
